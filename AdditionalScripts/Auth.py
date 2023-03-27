bcff48ba2f04e24f5b012c46ea712629d890099c
Alexandr Chernyaev <chernyaev.tmn@gmail.com>
2023-03-27 13:35:07 +0500

    Update Auth.py
    Deleting keys

--- AdditionalScripts/Auth.py
+++ AdditionalScripts/Auth.py
@@ -8,16 +8,16 @@ import tweepy as twp
 """
 api - access through API 1.1
 """
-auth = twp.AppAuthHandler(consumer_key='X2V4I8HZixP14UbbrghHxa32v',
-                          consumer_secret='b188hLoz78FgvQMKTnur43y8qKKa9kjkvBodHjcErThKYXUxId')
+auth = twp.AppAuthHandler(consumer_key='',
+                          consumer_secret='')
 
 api = twp.API(auth, wait_on_rate_limit=True)
 
 """
 apiP - access through API 1.1 *Premium*
 """
-authP = twp.AppAuthHandler(consumer_key='X2V4I8HZixP14UbbrghHxa32v',
-                           consumer_secret='b188hLoz78FgvQMKTnur43y8qKKa9kjkvBodHjcErThKYXUxId')
+authP = twp.AppAuthHandler(consumer_key='',
+                           consumer_secret='')
 
 apiP = twp.API(authP, wait_on_rate_limit=True)
 
@@ -25,7 +25,6 @@ apiP = twp.API(authP, wait_on_rate_limit=True)
 client - access through API 2.0
 """
 client = twp.Client(
-    bearer_token='AAAAAAAAAAAAAAAAAAAAAHu56QAAAAAAHGPUMk22dqgUOcZh8vXGPWm9oQ4'
-                 '%3DtMm5aGRcKBXMzai7D6FCsKLWITMvavIZNjwTv5fQLw8yRj2awd',
-    consumer_key='X2V4I8HZixP14UbbrghHxa32v',
-    consumer_secret='b188hLoz78FgvQMKTnur43y8qKKa9kjkvBodHjcErThKYXUxId', wait_on_rate_limit=True)
+    bearer_token='',
+    consumer_key='',
+    consumer_secret='', wait_on_rate_limit=True)
