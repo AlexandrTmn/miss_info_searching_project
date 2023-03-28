ea51930ea1642cd5dd73279fb31bd612b92a39b6
AlexandrTmn <chernyaev.tmn@gmail.com>
2023-03-28 11:10:34 +0500

    Format updates

--- AdditionalScripts/Learning/Model_fit.py
+++ AdditionalScripts/Learning/Model_fit.py
@@ -1,7 +1,7 @@
 import datetime
 import json
 import warnings
-
+import os, glob
 from joblib import load
 import pandas as pd
 from imblearn.over_sampling import SMOTE
@@ -15,11 +15,8 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
-
 from AdditionalScripts.DBScripts.DBConnection import db_connection
-
-# from sklearn.metrics import classification_report
-
+from sklearn.metrics import classification_report
 
 warnings.filterwarnings("ignore")
 
@@ -49,24 +46,31 @@ def get_data():
     return data_pd
 
 
+# Loading data from DB
 data = get_data()
+
 # drop nan columns and rows
 data = data.drop(columns=['UMData', 'TMMData', 'TMData'])
 data1 = data.dropna()
-# data1 = data1[0:10000]
 
+# Choosing X and y
 X = data1.drop('Hand_mark', axis=1)
 y = data1['Hand_mark']
-standardScale = StandardScaler()
+
+# Scaling and normalising
 label_Encoder = LabelEncoder()
-print(len(X), len(y))
+standardScale = StandardScaler()
 X['TMData.TM_Sentiment'] = label_Encoder.fit_transform(X['TMData.TM_Sentiment'])
 standardScale.fit_transform(X)
+# Splitting on test and training datasets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.40, random_state=128)
+
+# Applying SMOTE
 over_sampler = SMOTE(k_neighbors=3)
 X, y = over_sampler.fit_resample(X_train, y_train)
-print(len(X), len(y))
 # X, y = X_train,y_train
+
+# Defining models
 print('Data ready')
 k_neighbors = KNeighborsClassifier(n_neighbors=7, leaf_size=37, p=1)
 tree = DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=None, min_samples_split=3,
@@ -74,48 +78,53 @@ tree = DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=None,
 SVR = SVC(kernel='rbf', C=0.001, gamma=1.7782794100389227e-07)
 nn = MLPClassifier(activation='relu', hidden_layer_sizes=130, solver='adam', alpha=0.0004, learning_rate='invscaling')
 cnb = ComplementNB(alpha=0.17, norm=True)
+
+# Feature selection
 print('Selector start')
-selnei = SequentialFeatureSelector(k_neighbors, n_features_to_select=2).fit(X, y)
-seltree = SequentialFeatureSelector(tree, n_features_to_select=7).fit(X, y)
-selsvr = SequentialFeatureSelector(SVR, n_features_to_select=7).fit(X, y)
-selnn = SequentialFeatureSelector(nn, n_features_to_select=7).fit(X, y)
-selcnb = SequentialFeatureSelector(cnb, n_features_to_select=7).fit(X, y)
+sel_nei = SequentialFeatureSelector(k_neighbors, n_features_to_select=2).fit(X, y)
+sel_tree = SequentialFeatureSelector(tree, n_features_to_select=7).fit(X, y)
+sel_svr = SequentialFeatureSelector(SVR, n_features_to_select=7).fit(X, y)
+sel_nn = SequentialFeatureSelector(nn, n_features_to_select=7).fit(X, y)
+sel_cnb = SequentialFeatureSelector(cnb, n_features_to_select=7).fit(X, y)
 
 date = datetime.datetime.today()
 
-s1 = dump(selnei, 'selector_nei({}-{}).joblib'.format(str(date.day), str(date.hour)))
-s2 = dump(seltree, 'selector_tree({}-{}).joblib'.format(str(date.day), str(date.hour)))
-s3 = dump(selsvr, 'selector_svr({}-{}).joblib'.format(str(date.day), str(date.hour)))
-s4 = dump(selnn, 'selector_nn({}-{}).joblib'.format(str(date.day), str(date.hour)))
-s5 = dump(selcnb, 'selector_cnb({}-{}).joblib'.format(str(date.day), str(date.hour)))
+# Saving features
+dump(sel_nei, 'selector_nei({}-{}).joblib'.format(str(date.day), str(date.hour)))
+dump(sel_tree, 'selector_tree({}-{}).joblib'.format(str(date.day), str(date.hour)))
+dump(sel_svr, 'selector_svr({}-{}).joblib'.format(str(date.day), str(date.hour)))
+dump(sel_nn, 'selector_nn({}-{}).joblib'.format(str(date.day), str(date.hour)))
+dump(sel_cnb, 'selector_cnb({}-{}).joblib'.format(str(date.day), str(date.hour)))
 
+# Applying features selections
 print('Selector done\nTransform start')
-
-import os, glob
-
 features = 9
 folder_path = 'F:\Аспирант\Проект\AdditionalScripts\Learning\Logs\\''' + str(features) + '_features'
 sel1, sel2, sel3, sel4, sel5 = glob.glob(os.path.join(folder_path, '*.joblib'))
-selcnb, selnei, selnn, selsvr, seltree = load(sel1), load(sel2), load(sel3), load(sel4), load(sel5)
-selcnb_x, selnei_x, selnn_x, selsvr_x, seltree_x = selcnb.transform(X), selnei.transform(X), \
-    selnn.transform(X), selsvr.transform(X), seltree.transform(X)
+sel_cnb, sel_nei, sel_nn, sel_svr, sel_tree = load(sel1), load(sel2), load(sel3), load(sel4), load(sel5)
+sel_cnb_x, sel_nei_x, sel_nn_x, sel_svr_x, sel_tree_x = sel_cnb.transform(X), sel_nei.transform(X), \
+    sel_nn.transform(X), sel_svr.transform(X), sel_tree.transform(X)
 
 print('Transform ended\nFit start')
-with parallel_backend('threading', n_jobs=4):
-    # knf = k_neighbors.fit(selnei_x, y)
-    # svrf = SVR.fit(selsvr_x, y)
-    # cnbf = cnb.fit(selcnb_x, y)
-    # nnf = nn.fit(selnn_x, y)
-    # treef = tree.fit(seltree_x, y)
 
-    knf = k_neighbors.fit(X, y)
-    svrf = SVR.fit(X, y)
-    cnbf = cnb.fit(X, y)
+# Fitting models
+with parallel_backend('threading', n_jobs=4):
+    # kn = k_neighbors.fit(sel_nei_x, y)
+    # svr = SVR.fit(sel_svr_x, y)
+    # cnb = cnb.fit(sel_cnb_x, y)
+    # nn = nn.fit(sel_nn_x, y)
+    # tree = tree.fit(sel_tree_x, y)
+
+    kn = k_neighbors.fit(X, y)
+    svr = SVR.fit(X, y)
+    cnb = cnb.fit(X, y)
     nnf = nn.fit(X, y)
-    treef = tree.fit(X, y)
+    tree = tree.fit(X, y)
+
+# Saving models
 print('Fit ended\nSaving')
-s1 = dump(knf, filename='knf({}-{}).joblib'.format(str(date.day), str(date.hour)))
-s2 = dump(svrf, filename='svrf({}-{}).joblib'.format(str(date.day), str(date.hour)))
-s3 = dump(cnbf, filename='cnbf({}-{}).joblib'.format(str(date.day), str(date.hour)))
-s4 = dump(nnf, filename='nnf({}-{}).joblib'.format(str(date.day), str(date.hour)))
-s5 = dump(treef, filename='treef({}-{}).joblib'.format(str(date.day), str(date.hour)))
+dump(kn, filename='kn({}-{}).joblib'.format(str(date.day), str(date.hour)))
+dump(svr, filename='svr({}-{}).joblib'.format(str(date.day), str(date.hour)))
+dump(cnb, filename='cnb({}-{}).joblib'.format(str(date.day), str(date.hour)))
+dump(nnf, filename='nn({}-{}).joblib'.format(str(date.day), str(date.hour)))
+dump(tree, filename='tree({}-{}).joblib'.format(str(date.day), str(date.hour)))
--- AdditionalScripts/Legacy scripts/Balansing batch.py
+++ AdditionalScripts/Legacy scripts/Balansing batch.py
@@ -62,18 +62,12 @@ label_Encoder = LabelEncoder()
 X['TMData.TM_Sentiment'] = label_Encoder.fit_transform(X['TMData.TM_Sentiment'])
 standardScale.fit_transform(X)
 
-# over_sampler = RandomOverSampler(random_state=42)
-
 # Train / Test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-# X_train, y_train = over_sampler.fit_resample(X_train, y_train)
-print("X_train:", X_train.shape)
-print("y_train:", y_train.shape)
-print("X_test:", X_test.shape)
-print("y_test:", y_test.shape)
+
 
 # Fixing over sampling
-def build_and_test(X_tr, X_te, y_tr, y_te, class_weight=None, threshold=False):
+def build_and_test(X_tr, X_te, y_tr, y_te, class_weight=None):
     # Build and Plot PCA
     pca = PCA(n_components=14)
     pca.fit(X_tr)
@@ -83,7 +77,8 @@ def build_and_test(X_tr, X_te, y_tr, y_te, class_weight=None, threshold=False):
     if class_weight:
         model = SVC(gamma=0.001, C=1.7782794100389227e-07, kernel='rbf', class_weight=class_weight)
     else:
-        model = DecisionTreeClassifier()
+        model = DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=None, min_samples_split=3,
+                                       min_samples_leaf=2)
     model.fit(X_tr, y_tr)
 
     # Test the model
@@ -93,56 +88,31 @@ def build_and_test(X_tr, X_te, y_tr, y_te, class_weight=None, threshold=False):
     print('F1-score score %s' % f1_score(y_te, y_pred))
     print('Accuracy score %s' % accuracy_score(y_te, y_pred))
 
-    y_score = model.predict_proba(X_te)
-    fpr0, tpr0, thresholds = roc_curve(y_te, y_score[:, 1])
-    roc_auc0 = auc(fpr0, tpr0)
-
-    best_threshold = None
-    if threshold:
-        J = tpr0 - fpr0
-        ix = argmax(J)
-        best_threshold = thresholds[ix]
-
     # Print a classification report
     print(classification_report(y_te, y_pred))
-    return roc_auc0, fpr0, tpr0, best_threshold
+    return 0
 
 
 # choice of model for imbalance data
 print('imb')
-roc_auc_imb, fpr_imb, tpr_imb, _ = build_and_test(X_train, X_test, y_train, y_test)
-random_over_sample = RandomOverSampler()
+imb = build_and_test(X_train, X_test, y_train, y_test)
+
 print('ROS')
+random_over_sample = RandomOverSampler(random_state=3)
 X_res, y_res = random_over_sample.fit_resample(X_train, y_train)
-roc_auc_ros, fpr_ros, tpr_ros, _ = build_and_test(X_res, X_test, y_res, y_test)
+ROS = build_and_test(X_res, X_test, y_res, y_test)
+
 print('NM')
 under_sampler = NearMiss()
 X_res, y_res = under_sampler.fit_resample(X_train, y_train)
-roc_auc_nm, fpr_nm, tpr_nm, _ = build_and_test(X_res, X_test, y_res, y_test)
+NM = build_and_test(X_res, X_test, y_res, y_test)
+
 print('RUS')
-random_under_sampler = RandomUnderSampler(random_state=42)
+random_under_sampler = RandomUnderSampler(random_state=3)
 X_res, y_res = random_under_sampler.fit_resample(X_train, y_train)
-roc_auc_rus, fpr_rus, tpr_rus, _ = build_and_test(X_res, X_test, y_res, y_test)
+RUS = build_and_test(X_res, X_test, y_res, y_test)
+
 print('SMOTE')
 over_sampler = SMOTE(k_neighbors=3)
-X_res, y_res = over_sampler.fit_resample(X_train, y_train)
-roc_auc_smote, fpr_smote, tpr_smote, _ = build_and_test(X_res, X_test, y_res, y_test)
-print(_)
-print('Thr')
-roc_auc_thr, fpr_thr, tpr_thr, threshold = build_and_test(X_train, X_test, y_train, y_test, threshold=True)
-
-plt.plot(fpr_imb, tpr_imb, lw=3, label='Imbalanced $AUC_0$ = %.3f' % (roc_auc_imb))
-plt.plot(fpr_ros, tpr_ros, lw=3, label='ROS $AUC_0$ = %.3f' % (roc_auc_ros))
-plt.plot(fpr_smote, tpr_smote, lw=3, label='SMOTE $AUC_0$ = %.3f' % (roc_auc_smote))
-plt.plot(fpr_rus, tpr_rus, lw=3, label='RUS $AUC_0$ = %.3f' % (roc_auc_rus))
-plt.plot(fpr_nm, tpr_nm, lw=3, label='NM $AUC_0$ = %.3f' % (roc_auc_nm))
-plt.plot(fpr_thr, tpr_thr, lw=3, label='NM $AUC_0$ = %.3f' % (roc_auc_thr))
-plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate', fontsize=16)
-plt.ylabel('True Positive Rate', fontsize=16)
-plt.title('ROC curve', fontsize=16)
-plt.legend(loc="lower right", fontsize=14, frameon=False)
-plt.tick_params(axis='both', which='major', labelsize=16)
-plt.show()
+X, y = over_sampler.fit_resample(X_train, y_train)
+smote = build_and_test(X, X_test, y, y_test)
